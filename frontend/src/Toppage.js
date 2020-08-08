4bf37de1a19c23e0b7360c7757ef53dbd96e9a68
aweglteo <awegl.teo@gmail.com>
2020-08-08 13:58:57 +0900

    include express server and using SSR(server side rendering)

---
+++ frontend/src/Toppage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function TopPage() {
+  return (
+    <div className="topage">
+      <header className="App-header">
+        <div>Hello This is Toppage!</div>
+      </header>
+    </div>
+  );
+}
+
+export default TopPage;
